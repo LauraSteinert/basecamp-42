28d8fccdfdccc9de7532d114d05d0c67927e0a1e
lsteiner <lsteiner@student.42sp.org.br>
2021-04-02 13:50:55 +0000

    exercicio dois na malemolencia

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsteiner <lsteiner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/02 12:34:51 by lsteiner          #+#    #+#             */
-/*   Updated: 2021/04/02 13:48:10 by lsteiner         ###   ########.fr       */
+/*   Created: 2021/04/02 13:46:38 by lsteiner          #+#    #+#             */
+/*   Updated: 2021/04/02 13:50:25 by lsteiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char now;
 
-	now = 'a';
-	while (now <= 'z')
+	now = 'z';
+	while (now >= 'a')
 	{
 		write(STDOUT_FILENO, &now, 1);
-		now++;
+		now--;
 	}
 }
