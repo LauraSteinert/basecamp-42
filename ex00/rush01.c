d38e4c32430b943a9a4bf0bc360690850ac60fe4
pyago-ra <pyago-ra@student.42sp.org.br>
2021-04-02 23:58:18 +0000

    Falta arrumar quando for linha

--- ex00/rush01.c
+++ ex00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: pyago-ra <pyago-ra@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 22:28:52 by lsteiner          #+#    #+#             */
-/*   Updated: 2021/04/02 23:23:26 by pyago-ra         ###   ########.fr       */
+/*   Updated: 2021/04/02 23:56:36 by pyago-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,26 @@ void	rush(int x, int y)
 	int x_max; /* Máximo de largura */
 	int y_max; /* Máximo de altura*/
 
-	x_max = x; // 5
-	y_max = y; // 3
+	x_max = x;
+	y_max = y;
+
+	//if((x_max == 1 && y_max != 1) || (x_max != 1 && y_max == 1))
+
 	while(y > 0) // enquanto tiver altura pra ir, faz isso
 	{
 		while(x >= 0) //enquanto tiver largura pra ir, faz isso
 		{
-			if((x == x_max && y == y_max) || (x == 1 && y == 1))
+			if(x == 0)
+			{
+				ft_putchar('\n');
+			}
+			else if((x == x_max && y == y_max) || (x == 1 && y == 1))
 			{
 				ft_putchar('/');
 			}
 			else if((x == 1 && y == y_max) || (x == x_max && y == 1)) 
 			{
 				ft_putchar('\\'); 
-			}
-			else if(x == 0)
-			{
-				ft_putchar('\n');
 			}
 			else if ((x == x_max || x == 1) || (y == y_max || y == 1))
 			{
@@ -49,7 +52,7 @@ void	rush(int x, int y)
 			{
 				ft_putchar(' ');
 			}
-			x--;
+		x--;
 		}
 		x = x_max;
 		y--;
