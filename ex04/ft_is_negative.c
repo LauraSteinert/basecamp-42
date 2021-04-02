23af453306f3524a24070f4064283f21f2d93009
lsteiner <lsteiner@student.42sp.org.br>
2021-04-02 14:15:30 +0000

    esse 4 foi easy em

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsteiner <lsteiner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/02 14:10:01 by lsteiner          #+#    #+#             */
+/*   Updated: 2021/04/02 14:15:13 by lsteiner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char negative;
+	char positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n < 0)
+	{
+		write(STDOUT_FILENO, &negative, 1);
+	}
+	else if (n > 0)
+	{
+		write(STDOUT_FILENO, &positive, 1);
+	}
+}
