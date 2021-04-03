3707b5316ac66e1e4719851f6ca3fcea8cfcb0fd
lde-fari <lde-fari@student.42sp.org.br>
2021-04-03 22:51:51 +0000

    add arquivos

---
+++ ex00/rush02.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lde-fari <lde-fari@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/03 01:53:38 by lde-fari          #+#    #+#             */
+/*   Updated: 2021/04/03 22:33:12 by lde-fari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_verify(int col_x, int lin_y, int x, int y)
+{
+	if ((col_x == y || col_x == 1) && (lin_y == 1))
+	{
+		ft_putchar('A');
+	}
+	else if ((col_x == y || col_x == 1) && (lin_y == x))
+	{
+		ft_putchar('C');
+	}
+	else if ((col_x < y && col_x > 1) && (lin_y < x && lin_y > 1))
+	{
+		ft_putchar(' ');
+	}
+	else
+	{
+		ft_putchar('B');
+	}
+}
+
+void	rush(int x, int y)
+{
+	int col_x;
+	int lin_y;
+
+	col_x = 1;
+	while (col_x <= y)
+	{
+		lin_y = 1;
+		while (lin_y <= x)
+		{
+			ft_verify(col_x, lin_y, x, y);
+			lin_y = lin_y + 1;
+		}
+		ft_putchar('\n');
+		col_x = col_x + 1;
+	}
+}
