c4c07bf10eacae03e9c0870663236ac64c322a78
lsteiner <lsteiner@student.42sp.org.br>
2021-04-03 15:47:58 +0000

    exercicio 7 entendido e finalizado amem

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,13 +6,26 @@
 /*   By: lsteiner <lsteiner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 02:37:33 by lsteiner          #+#    #+#             */
-/*   Updated: 2021/04/03 03:25:22 by lsteiner         ###   ########.fr       */
+/*   Updated: 2021/04/03 15:46:32 by lsteiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putnbr(int nb){
-	
-	write(1,&nb, 1);
+void	ft_putnbr(int nb)
+{
+	if (nb < -nb)
+	{
+		nb = -nb;
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		nb = nb + '0';
+		write(1, &nb, 1);
+	}
 }
