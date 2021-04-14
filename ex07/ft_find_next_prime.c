4de02b7536f4f09cff9467685b58440190299863
lsteiner <lsteiner@student.42sp.org.br>
2021-04-14 22:52:47 +0000

    Vamo q vamo

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -6,11 +6,41 @@
 /*   By: lsteiner <lsteiner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 19:44:51 by lsteiner          #+#    #+#             */
-/*   Updated: 2021/04/14 19:45:06 by lsteiner         ###   ########.fr       */
+/*   Updated: 2021/04/14 22:51:48 by lsteiner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_find_next_prime(int nb)
+int		ft_is_prime(int nb)
 {
-	
+	int i;
+	int divisible;
+
+	i = 1;
+	divisible = 0;
+	if (nb == 1 || nb == 0 || nb < 0)
+		return (0);
+	while (i <= nb)
+	{
+		if ((nb % i) == 0)
+			divisible++;
+		i++;
+	}
+	if (divisible == 2)
+		return (1);
+	else
+		return (0);
+}
+
+int		ft_find_next_prime(int nb)
+{
+	int i;
+
+	i = 1;
+	while (i < 2 * nb)
+	{
+		if (ft_is_prime(i))
+			return (i);
+		i++;
+	}
+	return (i);
 }
